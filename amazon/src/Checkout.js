114fdd8e6766f4382f42ac8c85e2e5ec90643c0e
Ayush <kumarayush9694@gmail.com>
2023-07-18 23:27:21 +0530

    checkout page

--- amazon/src/Checkout.js
+++ amazon/src/Checkout.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./Checkout.css";
 import Subtotal from "./Subtotal.js";
+import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
 function Checkout() {
+  const [{basket},dispatch]=useStateValue();
   return (
     <div className="checkout">
       <div className="checkout__left">
-        <h2 className="checkout__title">Your Amazon Cart is empty</h2>
+        <h2 className="checkout__title">Your Amazon Cart </h2>
+        {basket.map(item=>(
+          <CheckoutProduct
+            id={item.id}
+            title={item.title}
+            image={item.image}
+            price={item.price}
+            rating={item.rating}
+          />
+        ))}
       </div>
       <div className='"checkout__right'>
         <Subtotal />
--- amazon/src/Home.js
+++ amazon/src/Home.js
@@ -15,6 +15,7 @@ function Home() {
 
         <div className="home_row">
           <Product
+            id="164615765"
             title="OnePlus Nord CE 2 Lite 5G (Black Dusk, 6GB RAM, 128GB Storage)"
             price={17+","+999}
             
@@ -22,18 +23,21 @@ function Home() {
             rating={3}
           />
           <Product
+            id="164614534"
             title="OnePlus 11 5G (Eternal Green, 8GB RAM, 128GB Storage)"
             price={56+","+999}
             image="https://m.media-amazon.com/images/I/61amb0CfMGL._SX679_.jpg"
             rating={4}
           />
           <Product
+            id="16461685"
             title="OnePlus Nord 3 5G (Misty Green, 8GB RAM, 128GB Storage)"
             price={33+","+999}
             image="https://m.media-amazon.com/images/I/61ngnTanGnL._SX679_.jpg"
             rating={3}
           />
           <Product
+            id="164614435"
             title="Apple iPhone 14 (128 GB) - Blue"
             price={66+","+999}
             image="https://m.media-amazon.com/images/I/61bK6PMOC3L._SX679_.jpg"
@@ -42,30 +46,35 @@ function Home() {
         </div>
         <div className="home_row">
           <Product
+            id="164611231"
             title=" OnePlus Nord Buds True Wireless in Ear Earbuds with Mic (Black Slate)"
             price={2+","+798}
             image="https://m.media-amazon.com/images/I/514NPRZ1AQL._SX679_.jpg"
             rating={4}
           />
           <Product
+            id="164610908"
             title="OnePlus Bullets Z2 Bluetooth Wireless in Ear Earphones with Mic (Magico Black)"
             price={1+","+598}
             image="https://m.media-amazon.com/images/I/51UhwaQXCpL._SX679_.jpg"
             rating={3}
           />
           <Product
+            id="1646121"
             title="BL Flip 5 Wireless Portable Bluetooth Speaker (Without Mic, Blue)"
             price={5+","+997}
             image="https://m.media-amazon.com/images/I/71CC5cSzqoL._SX679_.jpg"
             rating={5}
           />
           <Product
+            id="164618643"
             title="boAt Rockerz 450 Bluetooth On Ear Headphones with Mic (Luscious Black)"
             price={1+","+298}
             image="https://m.media-amazon.com/images/I/51xxA+6E+xL._SX679_.jpg"
             rating={3}
           />
           <Product
+            id="1646112345"
             title="ASUS RGB Gaming Wired On Ear Headset  ROG Delta   ( White )"
             price={17+","+999}
             image="https://m.media-amazon.com/images/I/51XXYvbE5NL._SX679_.jpg"
@@ -74,6 +83,7 @@ function Home() {
         </div>
         <div className="home_row">
         <Product
+          id="16461345"
             title="Sony Bravia 164 cm (65 inches) 4K Ultra HD Smart LED Google TV KD-65X74K (Black)"
             price={74+","+990}
             image="https://m.media-amazon.com/images/I/81wxS8abrgL._SX679_.jpg"
